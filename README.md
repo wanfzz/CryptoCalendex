0a8fb7017274408a89e4e9b9249a0d0c3488db58
Deepwood <wanf233@gmail.com>
2024-04-12 23:28:04 +0800

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# CryptoCalendex
-CryptoCalendex is a blockchain calendar application designed specifically for Web3 practitioners, helping users easily track and manage project activities and events.
+## Installation
+```bash
+npm gas-query
+```
--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('hello');
+const Web3 = require('web3');
+
+// 初始化 Web3 实例
+const web3 = new Web3('https://eth-mainnet.alchemyapi.io/v2/Cl9JZPpOZYRf6UcQ6GopCeQAhSOvo_b5');
+
+/**
+ * 查询链上 gas 数据的函数
+ * @param {number} blockNumber 查询的区块号
+ * @returns {Promise<object>} 包含 gas 数据的对象
+ */
+async function getGasData(blockNumber) {
+  try {
+    // 获取指定区块号的区块信息
+    const block = await web3.eth.getBlock(blockNumber);
+    // 构造返回对象
+    const gasData = {
+      gasLimit: block.gasLimit,
+      gasUsed: block.gasUsed,
+      gasPrice: await web3.eth.getGasPrice()
+    };
+    
+    return gasData;
+  } catch (error) {
+    throw new Error('Failed to get gas data: ' + error.message);
+  }
+}
+
+module.exports = { getGasData };
